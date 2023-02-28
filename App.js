e449865a25970672bc8200c855527637327f1db3
mesomorph07 <118825521+mesomorph07@users.noreply.github.com>
2023-02-28 18:27:01 +0530

    add

--- App.js
+++ App.js
@@ -1,1 +1,28 @@
 
+function home(){
+    document.getElementById("img1").style.background = "url(./img.jpg)"
+    document.getElementById("img1").style.backgroundSize = "cover";
+    document.getElementById("img1").style.backgroundPosition = "center";
+    
+}
+
+function work(){
+    document.getElementById("img1").style.background = "url(./img1.jpg)"
+    document.getElementById("img1").style.backgroundSize = "cover";
+    document.getElementById("img1").style.backgroundPosition = "center";
+    
+}
+
+function blog(){
+    document.getElementById("img1").style.background = "url(./img2.jpg)"
+    document.getElementById("img1").style.backgroundSize = "cover";
+    document.getElementById("img1").style.backgroundPosition = "center";
+    
+}
+
+function about(){
+    document.getElementById("img1").style.background = "url(./img3.jpg)";
+    document.getElementById("img1").style.backgroundSize = "cover";
+    document.getElementById("img1").style.backgroundPosition = "center";
+    
+}
